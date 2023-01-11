34a52a7764ba37c1d925464c671473d52f005917
andmikpar <andymikeparks@gmail.com>
2023-01-11 23:13:53 +0000

    creates keyboard buttons programatically

--- react-hangman/src/components/keyboard.jsx
+++ react-hangman/src/components/keyboard.jsx
@@ -1,43 +1,155 @@
-const Keyboard = ({ setNameInput }) => {
-  function handleClick(event) {
-    setNameInput(event.target.innerText);
+const Keyboard = ({ setGuess, guess }) => {
+  function handleClick(guess) {
+    setGuess(guess.toLowerCase());
   }
 
+  const KeyboardArray = [
+    {
+      value: 'Q',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'W',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'E',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'R',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'T',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'Y',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'U',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'I',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'O',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'P',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'A',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'S',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'D',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'F',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'G',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'H',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'J',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'K',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'L',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'Z',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'X',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'C',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'V',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'B',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'N',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+    {
+      value: 'M',
+      className: 'keyboardButton',
+      onClick: handleClick,
+    },
+  ];
   return (
     <div className="keyboard">
-      <button
-        className="keyboardButton"
-        onClick={(event) => {
-          handleClick(event);
-        }}
-      >
-        A
-      </button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
-      <button className="keyboardButton">A</button>
+      {KeyboardArray.map((key) => {
+        return (
+          <button
+            value={key.value}
+            onClick={(e) => {
+              key.onClick(e.target.value);
+            }}
+            className={key.className}
+          >
+            {key.value}
+          </button>
+        );
+      })}
     </div>
   );
 };
